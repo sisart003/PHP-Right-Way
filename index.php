25f892d5ee97b888926fcb469bfed19803ff6ad2
CandyWife003 <toyang.1996@gmail.com>
2024-12-06 03:10:05 +0800

    Starting Point

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP | The Right Way</title>
+</head>
+<body>
+    
+
+
+<!-- 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 -->
+</body>
+</html>
