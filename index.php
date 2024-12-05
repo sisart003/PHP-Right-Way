0a2596f938c9e591e4a84c63abfafb8cdb76ad74
CandyWife003 <toyang.1996@gmail.com>
2024-12-06 07:36:11 +0800

    Data types

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php  declare(strict_types=1); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,16 +11,117 @@
 <h1>PHP The Right Way</h1>
 <!-- Basics -->
  <?php 
- 
     # Output
-    echo 'Hello, World!';
+    echo "Hello, World!";
+    echo '<br><hr>';
     
     #variables
+    echo '<h2>Variables</h2>';
     $name = 'Chrishart';
 
- ?>
+    # Constants
+    
+    define('STATUS_PAID', 'paid');
+    const STATUS_PAID2 = 'paid';
+    echo STATUS_PAID2;
+    echo '<br>';
+    echo STATUS_PAID;
+    echo '<br>';
+    echo PHP_VERSION;
+    echo '<br>';
 
- <h1><?= $name ?></h1>
+    # Variables Variables
+    $foo = 'bzzz';
+    $$foo = 'buzz';
+    echo $foo, $bzzz;
+    echo '<br>';
+
+    echo '<hr>';
+
+    # Data Types & Type Casting
+    
+    echo '<h2>Data Types & Type Casting</h2>';
+
+    #scallable
+    $code = 123;
+    echo gettype($code);
+    echo '<br>';
+    var_dump($code);
+    echo '<br>';
+
+    #array
+    $pogChamp = ['Pog', 'KEKW', 'MONKA'];
+    var_dump($pogChamp);
+
+    echo '<br>';
+
+    #Integers
+    $x = PHP_INT_MAX;
+    echo $x;
+    echo '<br>';
+
+    # Floats
+    $fx = 13.5e-3;
+    $fx_max = PHP_FLOAT_MAX;
+    var_dump($fx);
+    echo '<br>';
+    echo $fx_max;
+    echo '<br>';
+
+    # Strings - Heredoc & Nowdoc
+    $text_heredoc = <<<TEXT
+    heredoc way
+    Line 1 $fx
+    Line 2 $x
+    Line 3
+    TEXT;
+
+    echo nl2br($text_heredoc);
+    echo '<br>';
+    echo '<br>';
+
+    $text_nowdoc = <<<'TEXT'
+    nowdoc way
+    Line 1
+    Line 2
+    Line 3
+    TEXT;
+    echo nl2br($text_nowdoc);
+    echo '<br>';
+
+    # Array
+    echo "<p>Array</p>";
+
+    # associative array
+    $common_pw = ['123', 'qwerty', '213456789', 'admin123'];
+    array_push($common_pw, '123456');
+
+    echo '<pre>';
+    var_dump($common_pw);
+    echo '</pre>';
+
+    echo '<br>';
+
+    echo $common_pw[2];
+    echo '<br>';
+
+    # Key Value pair
+    $common_names = [
+        'Danica' => 32,
+        'Janica' => 16,
+        'Hannah' => 29
+    ];
+
+    echo '<pre>';
+    var_dump($common_names);
+    echo '</pre>';
+
+    echo '<hr>';
+
+    # Expressions
+    echo "<h1>Expressions</h1>";
+    
+ ?>
 
 </body>
 </html>
