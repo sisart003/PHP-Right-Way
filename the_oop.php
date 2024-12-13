c21c67c62a7337b63a65684bfdf818f4f7bb3464
Sisart003 <chrishart.estrada@gmail.com>
2024-12-13 14:27:36 +0800

    Traits

--- the_oop.php
+++ the_oop.php
@@ -346,3 +346,158 @@
     // }
 
     // greeting($people);
+
+    # Traits
+    // trait Logger
+    // {
+    //     public function log($msg)
+    //     {
+    //         echo '<pre>';
+    //         echo date('Y-m-d h:i:s'). ':' . '(' . __CLASS__ . ') ' . $msg . '<br>';
+    //         echo '</pre>';
+    //     }
+    // }
+
+    // class BacnkAccount
+    // {
+    //     use Logger;
+
+    //     private $accountNumber;
+
+    //     public function __construct($accountNumber)
+    //     {
+    //         $this->accountNumber = $accountNumber;
+    //         $this->log("A new $accountNumber bank account created");
+    //     }
+
+    // }
+
+    // class User
+    // {
+    //     use Logger;
+
+    //     public function __construct()
+    //     {
+    //         $this->log("A new user created");
+    //     }
+    // }
+
+    # Using Multiple Traits
+    // trait Preprocessor
+    // {
+    //     public function preprocess()
+    //     {
+    //         echo "Preprocess.... done" . '<br>';
+    //     }
+    // }
+
+    // trait Compiler
+    // {
+    //     public function compile()
+    //     {
+    //         echo 'Compile code... done' . '<br>';
+    //     }
+    // }
+
+    // trait Assembler
+    // {
+    //     public function createObjCode()
+    //     {
+    //         echo 'Create the object code files... done' . '<br>';
+    //     }
+    // }
+
+    // trait Linker
+    // {
+    //     public function createExec()
+    //     {
+    //         echo 'Create the executable file... done' . '<br>';
+    //     }
+    // }
+
+    // class IDE
+    // {
+    //     use Preprocessor, Compiler, Assembler, Linker;
+
+    //     public function run()
+    //     {
+    //         $this->preprocess();
+    //         $this->compile();
+    //         $this->createObjCode();
+    //         $this->createExec();
+
+    //         echo 'Execute the file... done' . '<br>';
+    //     }
+    // }
+
+    // $ide = new IDE();
+    // $ide->run();
+
+    # Composing multiple traits
+    // trait Reader
+    // {
+    //     public function read($source)
+    //     {
+    //         echo sprintf('Read from %s <br>', $source);
+    //     }
+        
+    // }
+
+    // trait Writer
+    // {
+    //     public function write($destination)
+    //     {
+    //         echo sprintf('Write to $s <br>', $destination);
+    //     }
+    // }
+
+    // trait Copier
+    // {
+    //     use Reader, Writer;
+
+    //     public function copy($source, $destination)
+    //     {
+    //         $this->read($source);
+    //         $this->write($destination);
+    //     }
+    // }
+
+    // class FileUtil
+    // {
+    //     use Copier;
+
+    //     public function copyFile($source, $destination)
+    //     {
+    //         $this->copy($source, $destination);
+    //     }
+    // }
+
+    # Overriding trait method
+    // trait FileLogger
+    // {
+    //     public function log($msg)
+    //     {
+    //         echo 'File Logger ' . date('Y-m-d h:i:s') . ':' . $msg . '<br>';
+    //     }
+    // }
+
+    // trait DatabaseLogger
+    // {
+    //     public function log($msg)
+    //     {
+    //         echo 'Database Logger ' . date('Y-m-d h:i:s') . ':' . $msg . '<br>';
+    //     }
+    // }
+
+    // class Logger
+    // {
+    //     use FileLogger, DatabaseLogger{
+    //         # Aliasing trait method
+    //         DatabaseLogger::log as logToDatabase;
+    //         FileLogger::log insteadof DatabaseLogger;
+    //     }
+    // }
+
+    // $logger = new Logger();
+    // $logger->log('this is a test message #1');
+    // $logger->logToDatabase('this is a test message #2');
