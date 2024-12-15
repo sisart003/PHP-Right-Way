4f17879b850efa1498cd701df0ec1e13255336cf
Sisart003 <chrishart.estrada@gmail.com>
2024-12-15 22:36:07 +0800

    __toString() and __call()

--- the_oop.php
+++ the_oop.php
@@ -619,3 +619,62 @@
         // // show the attributes
         // echo $article->class; // light
         // echo $article->id; // main
+
+        # __toString()
+        // class BankAccount
+        // {
+        //     private $accountNumber;
+        //     private $balance;
+
+        //     public function __construct(
+        //         $accountNumber,
+        //         $balance
+        //     ){
+        //         $this->accountNumber = $accountNumber;
+        //         $this->balance = $balance;
+        //     }
+
+        //     public function __toString()
+        //     {
+        //         return "Bank Account: $this->accountNumber. Balance : $$this->balance";
+        //     }
+        // }
+
+        // $account = new BankAccount('1233544534', 200);
+        // echo $account;
+
+        # __call
+        // class Str
+        // {
+
+        //     private $s = '';
+
+        //     private $functions = [
+        //         'length' => 'strlen',
+        //         'upper' => 'strtoupper',
+        //         'lower' => 'strtolower'
+        //         // map more method to functions
+        //     ];
+
+        //     public function __construct(string $s)
+        //     {
+        //         $this->s = $s;
+        //     }
+
+        //     public function __call($method, $args)
+        //     {
+        //         if(!in_array($method, array_keys($this->functions))){
+        //             throw new BadMethodCallException();
+        //         }
+
+        //         array_unshift($args, $this->s);
+
+        //         return call_user_func_array($this->functions[$method], $args);
+        //     }
+
+        // }
+
+        // $s = new Str('Hello, World!');
+        // echo $s->upper() . '<br>';
+        // echo $s->lower() . '<br>';
+        // echo $s->length() . '<br>';
