ab8f67a73c8c46e9312698bb19e58addd4d57faf
Sisart003 <chrishart.estrada@gmail.com>
2024-12-17 02:54:02 +0800

    Compare Objects

--- the_oop.php
+++ the_oop.php
@@ -934,3 +934,43 @@
         // $alex->address->city = 'Northern';
 
         // var_dump($bob);
+
+        # Compare Objects
+        // class Point
+        // {
+        //     private $x;
+        //     private $y;
+
+        //     public function __construct($x, $y)
+        //     {
+        //         $this->x = $x;
+        //         $this->y = $y;
+        //     }
+        // }
+
+        // $p1 = new Point(10, 20);
+        // $p2 = new Point(10, 20);
+
+        // if($p1 === $p2){
+        //     echo 'p1 and p2 are equal.';
+        // }else{
+        //     echo 'p1 and p2 are not equal';
+        // }
+
+        // $p3 = $p2;
+
+        // if($p2 == $p3){
+        //     echo 'p2 and p3 are equal.';
+        // }else{
+        //     echo 'p2 and p3 are not equal.';
+        // }
+
+        // $p4 = new Point(20, 10);
+
+        // if($p3 == $p4){
+        //     echo 'p3 and p4 are equal';
+        // }else{
+        //     echo 'p3 and p4 are not equal';
+        // }
+
+        
