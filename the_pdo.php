6e0a2502cdbf1388afa36a1b223dc69e0661c320
Sisart003 <chrishart.estrada@gmail.com>
2024-12-21 02:25:38 +0800

    PDO IN statement

--- the_pdo.php
+++ the_pdo.php
@@ -133,3 +133,18 @@
     // }
 
     # PDO IN
+    // return an array of books with the book id in the list
+    // function get_book_list(\PDO $pdo, array $list) : array
+    // {
+    //     $placeholder = str_repeat('?,', count($list) - 1) . '?';
+
+    //     $sql = "SELECT book_id, title FROM books WHERE book_id IN ($placeholder)";
+
+    //     $statement = $pdo->prepare($sql);
+    //     $statement->execute($list);
+
+    //     return $statement->fetchAll(PDO::FETCH_ASSOC);
+    // }
+
+    // $books = get_book_list($pdo, [1, 2, 3]);
+    // print_r($books);
